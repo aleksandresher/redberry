d7fa08da2930d62b18d38c2860d67463c87393ce
aleksandresher <aleksandre.shervashidze.2@iliauni.edu.ge>
2022-07-09 23:08:20 +0400

    update

--- second.js
+++ second.js
@@ -6,56 +6,78 @@ const date = document.getElementById('date');
 const close = document.querySelector('.close');
 const required = document.querySelector('.error');
 
-// close.addEventListener('click', function () {
-//     document.querySelector('.errorMessagename').style.visibility = 'hidden';
-// });
-// username.addEventListener('click', function () {
-//     document.getElementById('name').style.backgroundColor = '#ffffff';
-//     document.getElementById('error').style.visibility = 'hidden';
-// })
+
+function checkInputs() {
+    // get the value from inputs
+    const usernamevalue = username.value.trim();
+    const emailvalue = email.value.trim();
+    const numbervalue = number.value.trim();
+    const datevalue = date.value;
+
+    if (usernamevalue === '') {
+        //show error
+        //add error class
+        setErrorFor(username);
+    } else if (emailvalue === '') {
+        setErrorFor(emailvalue);
+    } else if (numbervalue === '') {
+        setErrorFor(number);
+    } else if (datevalue === '') {
+        setErrorFor(date);
+    } else {
+        setSuccessFor(username);
+
+    }
+}
+
+function setErrorFor(input) {
+    document.querySelector('.errorMessagename').style.visibility = "visible";
+    document.getElementById('name').style.backgroundColor = "#ffefef";
+
+}
+
+
+const usernamevalue = username.value.trim();
+const emailvalue = email.value.trim();
+const numbervalue = number.value.trim();
+const birthDate = date.value.trim();
 
 
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     checkInputs();
-// });
 
-// // function myFunction() {
-// //     document.getElementById('email').placeholder = 'fdfdfdf';
-// // }
-// // emailof.addEventListener('click', myFunction());
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let number = document.getElementById('phone').value
+    let dateOfbirth = document.getElementById('date').value
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+            username: name,
+            emailofUser: email,
+            numberOfUser: Number(number),
+            userbirthdate: dateOfbirth
+        }),
+        headers: {
+            "Content-Type": "application/json; charset = UTF-8"
+        }
+    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+        })
 
 
 
+});
 
 
-// function checkInputs() {
-//     // get the value from inputs
-//     const usernamevalue = username.value.trim();
-//     const emailvalue = email.value.trim();
-//     const numbervalue = number.value.trim();
-//     const datevalue = date.value;
 
-//     if (usernamevalue === '') {
-//         //show error
-//         //add error class
-//         setErrorFor(username);
-//     } else if (emailvalue === '') {
-//         setErrorFor(emailvalue);
-//     } else if (numbervalue === '') {
-//         setErrorFor(number);
-//     } else if (datevalue === '') {
-//         setErrorFor(date);
-//     } else {
-//         setSuccessFor(username);
-
-//     }
-// }
-
-// function setErrorFor(input) {
-//     document.querySelector('.errorMessagename').style.visibility = "visible";
-//     document.getElementById('name').style.backgroundColor = "#ffefef";
 
 
 // }
@@ -96,39 +118,5 @@ const required = document.querySelector('.error');
 //         .then(data => console.log(data))
 //         .catch(err => console.log(err));
 // })
-const usernamevalue = username.value.trim();
-const emailvalue = email.value.trim();
-const numbervalue = number.value.trim();
-
-// const data = { usernamevalue, emailvalue, numbervalue };
-// const api = 'https://httpbin.org/post'
-
-// const options = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(data)
-// };
-
-// const response = await fetch('api', options);
-// const json = await response.json();
-const api = 'https://httpbin.org/post';
 
 
-const data = { username: usernamevalue };
-
-fetch('https://httpbin.org/post', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(usernamevalue),
-})
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
