3e37f0a4674880a9236f73243bc6102528003d9d
aleksandresher <aleksandre.shervashidze.2@iliauni.edu.ge>
2022-07-10 21:29:49 +0400

    update

--- second.js
+++ second.js
@@ -36,24 +36,46 @@ const showGamers = document.querySelector('.gamers');
 
 
 
-// const btnacc2 = document.querySelector('.accordeon');
-// const level = document.querySelector('.accordion');
+const btnacc2 = document.querySelector('.accordeon');
+const level = document.querySelector('.accordion');
 
-// btnacc2.addEventListener('click', function () {
-//     level.classList.toggle('hidden');
-// })
-
-
+btnacc2.addEventListener('click', function () {
+    level.classList.toggle('hidden');
+})
 
 
-// const btnacc = document.querySelector('.accordeon2');
-// const gamerso = document.querySelector('.gamers');
 
-// btnacc.addEventListener('click', function () {
-//     gamerso.classList.toggle('hidden');
-//     getResponse();
-// });
 
+const btnacc = document.querySelector('.accordeon2');
+const gamerso = document.querySelector('.gamers');
+
+btnacc.addEventListener('click', function () {
+    gamerso.classList.toggle('hidden');
+    getResponse();
+});
+
+let obj = [];
+const saveLevelInfo = function () {
+
+
+
+    document.querySelector('.begginer').addEventListener('click', function () {
+        obj.push('begginer');
+        return obj;
+    })
+    intermediateLevel.addEventListener('click', function () {
+        obj.push('intermediate');
+        return obj;
+    })
+    professionalLevel.addEventListener('click', function () {
+        obj.push('professional');
+        return obj;
+    })
+
+    console.log(obj);
+
+}
+saveLevelInfo();
 
 // window.onload = function () {
 
@@ -115,37 +137,35 @@ const showGamers = document.querySelector('.gamers');
 
 
 
-const myForm = document.getElementById('forma');
+// const myForm = document.getElementById('forma');
 
-myForm.addEventListener('submit', function (e) {
-    e.preventDefault();
+// myForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
 
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let number = document.getElementById('phone').value;
-    let dateOfbirth = document.getElementById('date').value;
-    fetch("https://jsonplaceholder.typicode.com/todos", {
-        method: "POST",
-        body: JSON.stringify({
-            username: name,
-            emailofUser: email,
-            numberOfUser: Number(number),
-            userbirthdate: dateOfbirth
-        }),
-        headers: {
-            "Content-Type": "application/json; charset = UTF-8"
-        }
-    })
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data)
-        })
+//     let name = document.getElementById('name').value;
+//     let email = document.getElementById('email').value;
+//     let number = document.getElementById('phone').value;
+//     let dateOfbirth = document.getElementById('date').value;
+//     fetch("https://jsonplaceholder.typicode.com/todos", {
+//         method: "POST",
+//         body: JSON.stringify({
+//             username: name,
+//             emailofUser: email,
+//             numberOfUser: Number(number),
+//             userbirthdate: dateOfbirth
+//         }),
+//         headers: {
+//             "Content-Type": "application/json; charset = UTF-8"
+//         }
+//     })
+//         .then(function (response) {
+//             return response.json()
+//         })
+//         .then(function (data) {
+//             console.log(data)
+//         })
 
-})
-
-
+// })
 
 
 
@@ -155,53 +175,55 @@ myForm.addEventListener('submit', function (e) {
 
 
 
-// function myfunc(event) {
-//     event.preventDefault();
-
-//     let usernamevalue = username.value.trim();
-//     let emailvalue = email.value.trim();
-//     let numbervalue = number.value.trim();
-//     let datevalue = date.value;
-
-//     localStorage.setItem('name', usernamevalue);
-//     localStorage.setItem('number', numbervalue);
-//     localStorage.setItem('email', emailvalue);
-// }
-// if () {
-//     document.getElementById('name').value = localStorage.getItem('name');
-// }
 
 
+// // function myfunc(event) {
+// //     event.preventDefault();
 
-// window.onload = function () {
-//     if (typeof (Storage) != 'undefined') {
-//         document.getElementById('name').value = localStorage.getItem('name');
-//         document.getElementById('email').value = localStorage.getItem('email');
-//         document.getElementById('phone').value = localStorage.getItem('number');
+// //     let usernamevalue = username.value.trim();
+// //     let emailvalue = email.value.trim();
+// //     let numbervalue = number.value.trim();
+// //     let datevalue = date.value;
+
+// //     localStorage.setItem('name', usernamevalue);
+// //     localStorage.setItem('number', numbervalue);
+// //     localStorage.setItem('email', emailvalue);
+// // }
+// // if () {
+// //     document.getElementById('name').value = localStorage.getItem('name');
+// // }
+
+
+
+// // window.onload = function () {
+// //     if (typeof (Storage) != 'undefined') {
+// //         document.getElementById('name').value = localStorage.getItem('name');
+// //         document.getElementById('email').value = localStorage.getItem('email');
+// //         document.getElementById('phone').value = localStorage.getItem('number');
+// //     }
+// // }
+
+
+// async function getResponse() {
+//     let response = await fetch('https://chess-tournament-api.devtest.ge/api/grandmasters');
+//     let content = await response.json();
+
+//     let list = document.querySelector('.post');
+
+//     let key;
+
+//     for (key in content) {
+
+//         list.innerHTML += `
+//     <li class='post'>
+//     <h4>${content[key].name}</h4 >
+//             <img src='https://chess-tournament-api.devtest.ge/${content[key].image}' class='imgOfgamers'></img>
+// </li > `
+
+//         content[key]
 //     }
-// }
 
-
-async function getResponse() {
-    let response = await fetch('https://chess-tournament-api.devtest.ge/api/grandmasters');
-    let content = await response.json();
-
-    let list = document.querySelector('.post');
-
-    let key;
-
-    for (key in content) {
-
-        list.innerHTML += `
-    <li class='post'>
-    <h4>${content[key].name}</h4 >
-            <img src='https://chess-tournament-api.devtest.ge/${content[key].image}' class='imgOfgamers'></img>
-</li > `
-
-        content[key]
-    }
-
-};
+// };
 
 
 
