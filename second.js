27780997ebfe21cc6c57a4cefef1ae5d7cf85e35
aleksandresher <aleksandre.shervashidze.2@iliauni.edu.ge>
2022-07-10 04:20:40 +0400

    update (save input value in localstorage)

--- second.js
+++ second.js
@@ -10,6 +10,25 @@ const showSecondAccordion = document.querySelector('.accordeon2')
 const showGamers = document.querySelector('.gamers');
 
 
+const btnacc2 = document.querySelector('.accordeon');
+const level = document.querySelector('.accordion');
+
+btnacc2.addEventListener('click', function () {
+    level.classList.toggle('hidden');
+})
+
+
+
+
+const btnacc = document.querySelector('.accordeon2');
+const gamerso = document.querySelector('.gamers');
+
+btnacc.addEventListener('click', function () {
+    gamerso.classList.toggle('hidden');
+    getResponse();
+});
+
+
 function checkInputs() {
     // get the value from inputs
     const usernamevalue = username.value.trim();
@@ -91,52 +110,51 @@ form.addEventListener('submit', function (e) {
 function myfunc(event) {
     event.preventDefault();
 
-    const usernamevalue = username.value.trim();
-    const emailvalue = email.value.trim();
-    const numbervalue = number.value.trim();
-    const datevalue = date.value;
+    let usernamevalue = username.value.trim();
+    let emailvalue = email.value.trim();
+    let numbervalue = number.value.trim();
+    let datevalue = date.value;
 
     localStorage.setItem('name', usernamevalue);
     localStorage.setItem('number', numbervalue);
     localStorage.setItem('email', emailvalue);
-
-    async function getResponse() {
-        let response = await fetch('https://jsonplaceholder.typicode.com/photos')
-        let content = await response.json()
-        content = content.splice(0, 5);
-
-        let list = document.querySelector('.post');
+}
 
 
-        let key;
+async function getResponse() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/photos')
+    let content = await response.json()
+    content = content.splice(0, 5);
 
-        for (key in content) {
+    let list = document.querySelector('.post');
 
-            list.innerHTML += `    
+
+    let key;
+
+    for (key in content) {
+
+        list.innerHTML += `    
     <li class='post'>
     <h4>${content[key].title}</h4 >
             <img src='${content[key].url}' class='imgOfgamers'></img>
 </li > `
 
-            content[key]
-        }
-
+        content[key]
     }
 
-    const btnacc2 = document.querySelector('.accordeon');
-    const level = document.querySelector('.accordion');
-
-    btnacc2.addEventListener('click', function () {
-        level.classList.toggle('hidden');
-    })
+}
 
 
 
 
-    const btnacc = document.querySelector('.accordeon2');
-    const gamerso = document.querySelector('.gamers');
+window.onbeforeunload = function () {
+    localStorage.setItem(savedusername, username.value());
+    localStorage.setItem(savedemail, emailof.value());
+    localStorage.setItem(savednumber, number.value());
+}
 
-    btnacc.addEventListener('click', function () {
-        gamerso.classList.toggle('hidden');
-        getResponse();
-    });
+window.onload = function () {
+    let username = localStorage.getItem(savedusername);
+    let email = localStorage.getItem(savedemail);
+    let number = localStorage.getItem(savednumber);
+}
